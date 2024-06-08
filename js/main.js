2a065ecb73822b0cf50d8e742469b993cd358ffe
Zahar <zaharseliverstov66@gmail.com>
2024-06-08 20:10:01 +0300

    refactor structure code

--- js/main.js
+++ js/main.js
@@ -1,34 +1,124 @@
-app = Vue.createApp({
+const app = Vue.createApp({
     data() {
         return {
             playerMotion: true,
-            field:
-                [
-                    false, false, false,
-                    false, false, false,
-                    false, false, false
-                ],
-            imageButton: ""
-
-        }
+            emptyImage: '/img/empty.png',
+            cross: '/img/nil.png',
+            nil: '/img/cross.png',
+            allImage: [],
+            backField: [],
+            movements: [],
+            animationTextureSwitch: true,
+            animationСounter: 5,
+            winAnimationInterval: null,
+            speedAnimation: 200
+        };
     },
     methods: {
-        RecordMove(index) {
-            field[index] = this.playerMotion;
-            this.playerMotion = !this.playerMotion;
-            
+        winAnimated() {
+            img = ''
             if (this.playerMotion) {
-                this.imageButton = "img/X.png"
+                img = this.animationTextureSwitch ? '/img/nilVeryDark.png' : '/img/nil.png';
             }
             else {
-                this.imageButton = "img/O.png"
+                img = this.animationTextureSwitch ? '/img/crossVeryDark.png' : '/img/cross.png';
+            }
+            this.animationTextureSwitch = !this.animationTextureSwitch;
+
+            this.allImage[this.movements[this.movements.length - 1]] = img;
+            this.allImage[this.movements[this.movements.length - 3]] = img;
+            this.allImage[this.movements[this.movements.length - 5]] = img;
+            this.animationСounter -= 1;
+
+            if (this.animationСounter <= 0) {
+                clearInterval(this.winAnimationInterval);
+                this.clearField();
             }
 
+        },
+        mouseHover(index) {
+            console.log("lox");
+            if (this.backField[index] == '') {
+                if (this.playerMotion) {
+                    this.allImage[index] = '/img/crossDark.png'
+                }
+                else {
+                    this.allImage[index] = '/img/nilDark.png'
+                }
+            }
+        },
+        mouseLeave(index) {
+            if (this.backField[index] == '') {
+                this.allImage[index] = this.emptyImage;
+            }
+        },
+        clearField() {
+            this.playerMotion = true;
+            this.allImage = [];
+            for (let index = 0; index < 9; index++) {
+                this.allImage.push(this.emptyImage);
+            }
+            this.backField =
+                [
+                    '', '', '',
+                    '', '', '',
+                    '', '', ''
+                ]
+            this.movements = [];
+            this.animationСounter = 5;
+            this.winAnimationInterval = null;
+        },
+        win() {
+            this.winAnimationInterval = setInterval(() => {
+                this.winAnimated();
+            }, this.speedAnimation);
+        },
+        checkFieldStatus(index) {
+
+            this.movements.push(index);
+
+            if (this.movements.length == 7) {
+                this.allImage[this.movements[0]] = this.emptyImage;
+                this.backField[this.movements[0]] = '';
+                this.movements.shift();
+            }
+            if (this.movements.length >= 6) {
+                if (this.playerMotion) {
+                    this.allImage[this.movements[0]] = '/img/crossDark.png';
+                }
+                else {
+                    this.allImage[this.movements[0]] = '/img/nilDark.png';
+                }
+            }
+            if ((this.backField[0] !== '' && this.backField[0] === this.backField[1] && this.backField[1] === this.backField[2]) ||
+                (this.backField[0] !== '' && this.backField[0] === this.backField[3] && this.backField[3] === this.backField[6]) ||
+                (this.backField[0] !== '' && this.backField[0] === this.backField[4] && this.backField[4] === this.backField[8]) ||
+                (this.backField[3] !== '' && this.backField[3] === this.backField[4] && this.backField[4] === this.backField[5]) ||
+                (this.backField[1] !== '' && this.backField[1] === this.backField[4] && this.backField[4] === this.backField[7]) ||
+                (this.backField[2] !== '' && this.backField[2] === this.backField[5] && this.backField[5] === this.backField[8]) ||
+                (this.backField[6] !== '' && this.backField[6] === this.backField[7] && this.backField[7] === this.backField[8]) ||
+                (this.backField[2] !== '' && this.backField[2] === this.backField[4] && this.backField[4] === this.backField[6])) {
+                this.win();
+            }
+        },
+        changeButtonImage(index) {
+            if (this.backField[index] == '') {
+                if (this.playerMotion) {
+                    this.allImage[index] = this.nil;
+                    this.backField[index] = 'o'
+                }
+                else {
+                    this.allImage[index] = this.cross;
+                    this.backField[index] = 'x'
+                }
+                this.playerMotion = !this.playerMotion;
+                this.checkFieldStatus(index);
+            }
         }
     },
-    create() {
-        this.playerMotion = true
-        
+    created() {
+        this.clearField();
     }
 });
-app.mount("#field")
+
+app.mount("#app");
